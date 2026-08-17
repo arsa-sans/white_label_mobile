a7aa2d447d2e4e7165edfefae1636728cee56ede
arsa-sans <arsaprayata72@gmail.com>
2026-08-18 06:51:02 +0700

    optimalization

--- lib/core/api/api_client.dart
+++
@@ -1,2 +0,0 @@
-// Backward-compatible re-export — logic telah dipindah ke core/network/api_client.dart
-export '../network/api_client.dart';
--- lib/core/api/api_endpoints.dart
+++
@@ -1,2 +0,0 @@
-// Backward-compatible re-export — logic telah dipindah ke core/constants/api_endpoints.dart
-export '../constants/api_endpoints.dart';
